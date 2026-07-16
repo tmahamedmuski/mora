ee7032d5bfb9220d8ac829a44b60ad60ef408a74
muskitma <tm.ahamedmuski@gmail.com>
2026-07-16 12:15:25 +0530

    Track frontend source files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Signup from './components/Signup';  // Import Signup component
+import './App.css';
+
+const App = () => {
+  return (
+    <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+      <div className="app-container">
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} /> {/* Add Signup route */}
+          <Route path="*" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
